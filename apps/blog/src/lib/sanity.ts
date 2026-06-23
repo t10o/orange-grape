c4a2d61e58421992c41abc49c430669ee020bb7c
t10o <dev@t10o.one>
2026-06-23 12:38:03 +0900

    環境変数が読み込めてなかったのを修正

--- apps/blog/src/lib/sanity.ts
+++ apps/blog/src/lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: import.meta.env.SANITY_PROJECT_ID,
-  dataset: import.meta.env.SANITY_DATASET ?? "production",
+  projectId: import.meta.env.SANITY_PROJECT_ID ?? process.env.SANITY_PROJECT_ID,
+  dataset: import.meta.env.SANITY_DATASET ?? process.env.SANITY_DATASET ?? "production",
   apiVersion: "2024-01-01",
   useCdn: true,
   perspective: "published",
